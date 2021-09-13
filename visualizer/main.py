709bec3442678a6a1783123847a4457272b30ed4
totospl <totospl99@gmail.com>
2021-09-13 23:07:37 +0200

    finilized visualizer
    Co-authored-by: Rafał Fiedosiuk <fiedosiukr@users.noreply.github.com>

--- visualizer/main.py
+++ visualizer/main.py
@@ -1,37 +1,12 @@
 from collections import defaultdict
+
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
 from matplotlib import pyplot as plt
-import numpy as np
 
 
-def main():
-    transport = RequestsHTTPTransport(url="http://localhost:4000/graphql")
-    client = Client(transport=transport, fetch_schema_from_transport=True)
+def price_to_build_year(client):
     query = gql("""
-    query {
-        salesAllGet {
-            date {
-                day
-                month
-                year
-            }
-            property {
-                taxClass
-                landSquareFeet
-                yearBuild
-            }
-            location {
-                borough
-                zipCode
-                neighborhood
-            }
-        }
-    }""")
-    # result = client.execute(query)
-
-    # does the price depends on the year build?
-    query_1 = gql("""
     query {
         salesAllGet {
             property {
@@ -41,14 +16,15 @@ def main():
         }
     }
     """)
-    result_1 = client.execute(query_1)["salesAllGet"]
+    result = client.execute(query)["salesAllGet"]
+
     def def_value():
         return [0.0, 0.0]
     year_prices = defaultdict(def_value)
 
     years = []
     prices = []
-    for entry in result_1:
+    for entry in result:
         year = int(entry["property"]["yearBuild"])
         price = int(entry["salePrice"])
         if year > 1500 and price > 100:
@@ -56,22 +32,19 @@ def main():
             average = year_prices[rounded_year]
             average[0] += 1
             average[1] += price
-    
+
     for i in range(1500, 2050, 10):
         if year_prices[i][0] == 0:
             continue
         price_avg = year_prices[i][1] / year_prices[i][0]
         years.append(i)
         prices.append(price_avg)
-    
-    plt.bar(years, prices, width=5)
-    plt.show()
+
+    return years, prices
 
 
-    # {'property': {'yearBuild': -1}, 'salePrice': -1}
-
-    # does the price depends on the zipcode? (location)
-    query_2 = gql("""
+def price_to_zipcode(client):
+    query = gql("""
     query {
         salesAllGet {
             location {
@@ -81,11 +54,152 @@ def main():
         }
     }
     """)
-    #result_2 = client.execute(query_2)
+    result = client.execute(query)["salesAllGet"]
 
-    # TODO: wykresiki
-    # cena od landsquare feet
-    # avg yearbuild od zipcode
+    def def_value():
+        return [0.0, 0.0]
+    zipcodes_prices = defaultdict(def_value)
+
+    prices = []
+    zipcodes = []
+    for entry in result:
+        zipcode = int(entry["location"]["zipCode"])
+        price = int(entry["salePrice"])
+        if zipcode != "undefined" and price > 100:
+            if int(zipcode) == 0:
+                continue
+            average = zipcodes_prices[int(zipcode)]
+            average[0] += 1
+            average[1] += price
+
+    for key, value in zipcodes_prices.items():
+        price_avg = value[1] / value[0]
+        zipcodes.append(key)
+        prices.append(price_avg)
+
+    return zipcodes, prices
+
+
+def price_to_area(client):
+    query = gql("""
+    query {
+        salesAllGet {
+            property {
+                landSquareFeet
+            }
+            salePrice
+        }
+    }
+    """)
+    result = client.execute(query)["salesAllGet"]
+
+    def def_value():
+        return [0.0, 0.0]
+    areas_prices = defaultdict(def_value)
+
+    areas = []
+    prices = []
+    for entry in result:
+        area = int(entry["property"]["landSquareFeet"])
+        price = int(entry["salePrice"])
+        if area > 10000 and price > 100:
+            rounded_area = area - (area % 250)
+            average = areas_prices[rounded_area]
+            average[0] += 1
+            average[1] += price
+
+    for i in range(10000, 17000, 250):
+        if areas_prices[i][0] == 0:
+            continue
+        price_avg = areas_prices[i][1] / areas_prices[i][0]
+        areas.append(i)
+        prices.append(price_avg)
+
+    return areas, prices
+
+
+def zipcode_to_yearbuilt(client):
+    query = gql("""
+    query {
+      salesAllGet {
+        property {
+          yearBuild
+        }
+        location {
+          zipCode
+        }
+      }
+    }
+    """)
+    result = client.execute(query)["salesAllGet"]
+
+    def def_value():
+        return [0.0, 0.0]
+    zipcodes_yearbuilts = defaultdict(def_value)
+
+    zipcodes = []
+    yearbuilts = []
+    for entry in result:
+        zipcode = int(entry["location"]["zipCode"])
+        year = int(entry["property"]["yearBuild"])
+        if zipcode != "undefined" and year > 1500:
+            if int(zipcode) == 0:
+                continue
+            average = zipcodes_yearbuilts[int(zipcode)]
+            average[0] += 1
+            average[1] += year
+
+    for key, value in zipcodes_yearbuilts.items():
+        yearbuilt_avg = value[1] / value[0]
+        zipcodes.append(key)
+        yearbuilts.append(yearbuilt_avg)
+
+    return zipcodes, yearbuilts
+
+
+def main():
+    transport = RequestsHTTPTransport(url="http://localhost:4000/graphql")
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    plt.figure(1, figsize=(800, 800))
+    ax = plt.gca()
+    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    ax.ticklabel_format(useOffset=False, style='plain')
+
+    subplot_1 = plt.subplot(221)
+    subplot_1.axes.ticklabel_format(
+        style='plain', useOffset=False, axis='both')
+    plt.bar(*price_to_build_year(client), width=5)
+    plt.title("Price of property depending on the year")
+    plt.xlabel("year")
+    plt.ylabel("price")
+
+    subplot_2 = plt.subplot(222)
+    subplot_2.axes.ticklabel_format(
+        style='plain', useOffset=False, axis='both')
+    plt.bar(*price_to_zipcode(client), width=5)
+    plt.title("Price of property depending on the zipcode")
+    plt.xlabel("zipcode")
+    plt.ylabel("price")
+
+    subplot_3 = plt.subplot(223)
+    subplot_3.axes.ticklabel_format(
+        style='plain', useOffset=False, axis='both')
+    plt.bar(*price_to_area(client), width=5)
+    plt.title("Price of property depending on the area")
+    plt.xlabel("area")
+    plt.ylabel("price")
+
+    subplot_4 = plt.subplot(224)
+    subplot_4.axes.ticklabel_format(
+        style='plain', useOffset=False, axis='both')
+    plt.ylim([1900, 2050])
+    plt.bar(*zipcode_to_yearbuilt(client), width=5)
+    plt.title("Yearbuilt depending on the zipcode")
+    plt.xlabel("zipcode")
+    plt.ylabel("yearbuilt")
+
+    plt.show()
 
 
 if __name__ == "__main__":
